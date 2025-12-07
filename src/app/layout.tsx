d378f0c41acd18318797051b86d027b4b9b92931
Md. Minar Munshi <minarmunshi46@gmail.com>
2025-12-08 00:19:24 +0600

    Scafold the Application

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+import { JetBrains_Mono } from 'next/font/google';
+import './globals.css';
+
+const jetbrainsMono = JetBrains_Mono({
+  variable: '--font-jetbrains-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Private Chat',
+  description:
+    'This is a private chat application which you can use to chat with your friends',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${jetbrainsMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { nanoid } from 'nanoid';
+import { useEffect, useState } from 'react';
+
+const ANIMALS = ['wolf', 'hawk', 'bear', 'shark'];
+const STORAGE_KEY = 'chat_username';
+
+const generateUsername = () => {
+  const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)];
+  return `anonymous-${word}-${nanoid(5)}`;
+};
+
+export default function HomePage() {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const main = () => {
+      const storedUsername = localStorage.getItem(STORAGE_KEY);
+      if (storedUsername) {
+        setUsername(storedUsername);
+        return;
+      }
+
+      const username = generateUsername();
+      localStorage.setItem(STORAGE_KEY, username);
+      setUsername(username);
+    };
+
+    main();
+  }, []);
+
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center p-4'>
+      <div className='w-full max-w-md space-y-8'>
+        <div className='space-y-2 text-center'>
+          <h1 className='text-2xl font-bold tracking-tight text-green-500'>
+            {'>'}private_chat
+          </h1>
+          <p className='text-sm text-zinc-500'>
+            A private, self-destructing chat room.
+          </p>
+        </div>
+
+        <div className='border border-zinc-800 bg-zinc-900/50 p-6 backdrop-blur-md'>
+          <div className='space-y-5'>
+            <div className='space-y-2'>
+              <label className='flex items-center text-zinc-500'>
+                Your Identity
+              </label>
+
+              <div className='flex items-center gap-3'>
+                <div className='flex-1 border border-zinc-800 bg-zinc-950 p-3 font-mono text-sm text-zinc-400'>
+                  {username}
+                </div>
+              </div>
+            </div>
+
+            <button className='mt-2 w-full cursor-pointer bg-zinc-100 p-3 text-sm font-bold text-black transition-colors hover:bg-zinc-50 hover:text-black disabled:opacity-50'>
+              CREATE SECURE ROOM
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
