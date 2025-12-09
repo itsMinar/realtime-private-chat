895cb7d4280ecbe1a695e3aeb90df2d06618061f
Md. Minar Munshi <minarmunshi46@gmail.com>
2025-12-09 19:17:40 +0600

    feat: Setup the auth middleware for api

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { redis } from '@/lib/redis';
 import { Elysia } from 'elysia';
 import { nanoid } from 'nanoid';
+import { z } from 'zod';
+import { authMiddleware } from './auth';
 
 const ROOM_TTL_SECONDS = 60 * 10;
 
@@ -17,7 +19,23 @@ const rooms = new Elysia({ prefix: '/room' }).post('/create', async () => {
   return { roomId };
 });
 
-const app = new Elysia({ prefix: '/api' }).use(rooms);
+const messages = new Elysia({ prefix: '/messages' }).use(authMiddleware).post(
+  '/',
+  ({ auth, body }) => {
+    const { sender, text } = body;
+  },
+  {
+    query: z.object({
+      roomId: z.string(),
+    }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    }),
+  }
+);
+
+const app = new Elysia({ prefix: '/api' }).use(rooms).use(messages);
 
 export const GET = app.fetch;
 export const POST = app.fetch;
