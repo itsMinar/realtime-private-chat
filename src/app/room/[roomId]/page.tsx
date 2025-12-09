eb98efa5df13655fdf9e95ca81aa1a5a7cc5de65
Md. Minar Munshi <minarmunshi46@gmail.com>
2025-12-09 11:46:46 +0600

    feat: Implement the Proxy

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -2,12 +2,14 @@
 
 import { formatTimeRemaining } from '@/lib/utils';
 import { useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function RoomPage() {
   const params = useParams();
   const roomId = params.roomId as string;
 
+  const [input, setInput] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
   const [copyStatus, setCopyStatus] = useState('COPY');
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
 
@@ -62,6 +64,37 @@ export default function RoomPage() {
           DESTROY NOW
         </button>
       </header>
+
+      {/* MESSAGES */}
+      <div className='scrollbar-thin flex-1 space-y-4 overflow-y-auto p-4'></div>
+
+      {/* new message */}
+      <div className='border-t border-zinc-800 bg-zinc-900/30 p-4'>
+        <div className='flex gap-4'>
+          <div className='group relative flex-1'>
+            <span className='absolute top-1/2 left-4 -translate-y-1/2 animate-pulse text-green-500'>
+              {'>'}
+            </span>
+            <input
+              autoFocus
+              type='text'
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' && input.trim()) {
+                  inputRef.current?.focus();
+                }
+              }}
+              placeholder='Type message...'
+              className='w-full border border-zinc-800 bg-black py-3 pr-4 pl-8 text-sm text-zinc-100 transition-colors placeholder:text-zinc-700 focus:border-zinc-700 focus:outline-none'
+            />
+          </div>
+
+          <button className='cursor-pointer bg-zinc-800 px-6 text-sm font-bold text-zinc-400 transition-all hover:text-zinc-200 disabled:cursor-not-allowed disabled:opacity-50'>
+            SEND
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
